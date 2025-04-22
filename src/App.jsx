b67614e083e6fa0a21d8011520ac9bf346e4b075
hectrhcc <hcontrerascorvacho@gmail.com>
2025-04-22 12:39:31 -0400

    Agregando proyecto React con Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+import React from 'react';
+
+function App() {
+
+  return (
+    <>
+      <h1>Página en Construcción</h1>
+    </>
+  )
+}
+
+export default App
