c7f948aeff20731e31d3cca60adef86a687cc175
hectrhcc <hcontrerascorvacho@gmail.com>
2025-05-10 17:55:45 -0400

    contacto sin funcionalidad

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 // ContactForm.jsx
 import { useForm } from 'react-hook-form';
 
@@ -14,12 +15,20 @@ export default function ContactForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4  mx-auto bg-amber-300  border-2 ">
-      <div className='flex mt-7 '>
+    
+      <div className=' md:flex md:flex-col  '>
+      
+       <div className='flex flex-col p-3 '>
+               <h3 className='text-2xl text-center font-semibold mb-3 color 7 '>¡Estamos aquí para ayudarte!</h3>
+                <p className='text-lg mb-3 ml-4 mr-3 text-left justify-between  md:mx-auto md:justify-center md:w-full '>Si tienes alguna pregunta o necesitas más información, no dudes en ponerte en contacto con nosotros. Rellena el siguiente formulario y te responderemos en breve.</p>
+        </div>
+      
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4  mx-auto  md:w-full md:h-full ">
+      <div className='flex  '>
       <div className="flex flex-col">
         <input
           {...register('nombre', { required: 'El nombre es obligatorio' })}
-          className=" w-10/12 rounded-2xl  ml-4 mr-1 p-1 bg-white pl-4"
+          className=" w-10/12 rounded-2xl  ml-4 mr-1 p-1 pl-4 bg-gray-100  border-1 md:w-75 "
           placeholder='Nombre'
         />
         {errors.nombre && <p className="text-red-500 text-sm w-full ml-5 ">{errors.nombre.message}</p>}
@@ -27,7 +36,7 @@ export default function ContactForm() {
         <div className="flex flex-col">
                <input
           {...register('apellido', { required: 'El apellido es obligatorio' })}
-          className="  w-10/12 rounded-2xl ml-3 mr-1 p-1  bg-white  pl-4"
+          className="  w-10/12 rounded-2xl ml-3 mr-1 p-1  pl-4 bg-gray-100  border-1 md:w-75"
           placeholder='Apellido'
         />
         {errors.apellido && <p className="text-red-500 text-sm  w-full ml-5  ">{errors.apellido.message}</p>}
@@ -38,7 +47,7 @@ export default function ContactForm() {
              <input
           type="email"
           {...register('correo', { required: 'El correo es obligatorio' })}
-          className=" w-10/12 rounded-2xl mr-1 ml-4 p-1  bg-white  pl-4"
+          className=" w-10/12 rounded-2xl mr-1 ml-4 p-1 pl-4 bg-gray-100  border-1 md:w-75"
           placeholder='Correo'
         />
         {errors.correo && <p className="text-red-500 text-sm  w-full ml-5  ">{errors.correo.message}</p>}
@@ -47,7 +56,7 @@ export default function ContactForm() {
                <input
           type="telefono"
           {...register('telefono', { required: 'El telefono es obligatorio' })}
-          className=" w-10/12 rounded-2xl ml-3 mr-1 p-1 bg-white  pl-4"
+          className=" w-10/12 rounded-2xl ml-3 mr-1 p-1 pl-4 bg-gray-100 border-1 md:w-75"
           placeholder='Teléfono'
         />
         {errors.telefono && <p className="text-red-500 text-sm w-full ml-5 ">{errors.telefono.message}</p>}
@@ -56,7 +65,7 @@ export default function ContactForm() {
       <div>
         <textarea
           {...register('mensaje', { required: 'El mensaje no puede estar vacío' })}
-          className=" min-w-11/12 rounded-2xl ml-3 mr-3 min-h-auto h-36  bg-white pl-4"
+          className=" min-w-11/12 rounded-2xl ml-3 mr-3 min-h-auto h-36  bg-gray-100 pl-4 border-1 "
           placeholder='Mensaje'
         />
         {errors.mensaje && <p className="text-red-500 text-sm block w-full pl-4">{errors.mensaje.message}</p>}
@@ -64,10 +73,11 @@ export default function ContactForm() {
 
       <button
         type="submit"
-        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 mb-4  ml-4 hover:scale-110 "
+        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700   ml-4 hover:scale-110 md:w-155 md:py-3 "
       >
         Enviar
       </button>
-    </form>
+      </form>
+   </div>
   );
 }
--- src/pages/ContactoPage.jsx
+++ src/pages/ContactoPage.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
 import Contacto from '../components/Contacto';
+import qrwsp from '../../public/qrcode-56967689934.jpg';
 
 const ContactoPage = () => {
   return (
     <>
       <h1 className='text-5xl font-bold color text-center mt-7' >Contacto</h1>
-      <hr className="w-1/2 h-1 mx-auto mt-2 mb-3 bg-amber-300 border-none rounded md:w-1/6 md:items-end" />
-    <div className='flex flex-col  w-full p-3'>
-    <h3 className='text-2xl text-center font-semibold mb-3 color 7 '>¡Estamos aquí para ayudarte!</h3>
-    <p className='text-lg mb-3 ml-4 mr-3 text-left justify-between  md:mx-auto md:text-center md:w-1/3 '>Si tienes alguna pregunta o necesitas más información, no dudes en ponerte en contacto con nosotros.</p>
-</div>
+        <hr className="w-1/2 h-1 mx-auto mt-2 mb-3 bg-amber-300 border-none rounded md:w-1/6 md:items-end" />
+    <div className=' md:flex  md:flex-row mb-4'>
+      <div className='hidden md:flex  w-3/12'>
+      <video
+       src="/contactman.mp4"
+      autoPlay
+      loop
+      muted
+      playsInline
+      className="w-full h-130 "
+      >
+      Tu navegador no soporta la reproducción de video.
+      </video>
+      </div>
+    <div className='  md:flex  md:w-6/12 mx-auto '>  
       <Contacto/>
+    </div>
+    <div className='hidden md:block  w-3/12 relative right-13'>
+                        <p className=' text-4xl font-extralight text-center mt-1  '>Whatsapp QR</p>
+              <img src={qrwsp} alt="whatsapp" className="hover:scale-105 mt-2 object-contain  mx-auto w-4/6  " />
+              <p className=' font-bold text-center mt-1  '> +56967689934</p>
+    </div>
+    </div>
+
+     
      
     </>
   )
