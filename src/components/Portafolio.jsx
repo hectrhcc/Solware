cd8ccd141b773b5546f99219bfa744b744faefee
hectrhcc <hcontrerascorvacho@gmail.com>
2025-05-15 21:54:38 -0400

    portfolio base

--- src/components/Portafolio.jsx
+++ src/components/Portafolio.jsx
@@ -1,17 +1,29 @@
 import React, { useState , useEffect} from 'react';
+import agricola from '../../public/agricola2.png';
+import tracker from '../../public/trackerip.png';
+import zona from '../../public/zonafotica.png';
 
 export default function Portafolio() {
 
 return (  
 <>
  <h1 className='text-2xl md:text-4xl lg:text-5xl font-bold color text-center mt-7 color md:mt-10 mb-5' >PORTAFOLIO</h1>
-    <p className='text-sm md:text-lg w-11/12 md:w-2/3 lg:w-1/2 mx-auto text-center lg:text-xl mb-5'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+    <p className='text-sm md:text-lg w-11/12 md:w-2/3 lg:w-full mx-auto text-center lg:text-xl mb-5'>
+        Conoce algunos de los proyectos que hemos desarrollado junto a nuestros clientes.
       </p>
-    <div className='mt-5 gap-5 flex flex-col lg:flex-row justify-center items-center w-full lg:w-3/4 mx-auto'>    
-        <div className='text-sm md:text-lg border-1 w-3/4 lg:w-1/2 p-6 text-center rounded shadow'>web 1</div>
-        <div className='text-sm md:text-lg border-1 w-3/4 lg:w-1/2 p-6 text-center rounded shadow'>web 2</div>
-        <div className='text-sm md:text-lg border-1 w-3/4 lg:w-1/2 p-6 text-center rounded shadow'>web 3</div>
+    <div className='mt-5 gap-5 flex flex-col lg:flex-row justify-center items-center w-full lg:w-3/4 mx-auto' data-aos="fade-up">    
+        <div className='text-sm md:text-lg border-1 w-3/4 lg:w-1/2 p-6 text-center rounded shadow'>
+        <img src={tracker} alt='tacker ip' className='w-full h-40 object-cover rounded '/>
+        <h2 className='text-lg font-semibold color mt-2'>Tracker IP</h2>
+        </div>
+        <div className='text-sm md:text-lg border-1 w-3/4 lg:w-1/2 p-6 text-center rounded shadow'>
+        <img src={agricola} alt='agricola del norte s.a' className='w-full h-40 object-cover rounded '/>
+        <h2 className='text-lg font-semibold color mt-2'>Agrícola del Norte S.A</h2>
+        </div>
+        <div className='text-sm md:text-lg border-1 w-3/4 lg:w-1/2 p-6 text-center rounded shadow'>
+        <img src={zona} alt='zona fotica' className='w-full h-40 object-cover rounded '/>
+        <h2 className='text-lg font-semibold color mt-2'>Zona Fótica</h2>
+        </div>
     </div>  
     
      
--- src/pages/PortafolioPage.jsx
+++ src/pages/PortafolioPage.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import Portafolio from '../components/Portafolio';
-
+import camara from '../../public/camarografo.png';
+import mascotas from '../../public/mascotas.png';
+import snap from '../../public/snap.png';
 const PortafolioPage = () => {
   return (
     <>
     
          <Portafolio/>
         {/*el resto del portafolio */}
-        <div className='mt-5 mb-5 gap-5 flex flex-col lg:flex-row justify-center items-center w-full lg:w-3/4 mx-auto'>
-      
-        <div className='border-1 w-3/4 lg:w-1/2 text-center p-6 text-center rounded shadow'>web 4</div>
-        <div className='border-1 w-3/4 lg:w-1/2 text-center p-6 text-center rounded shadow'>web 5</div>
-        <div className='border-1 w-3/4 lg:w-1/2 text-center p-6 text-center rounded shadow'>web 6</div>
+        <div className='mt-5 mb-5 gap-5 flex flex-col lg:flex-row justify-center items-center w-full lg:w-3/4 mx-auto' data-aos="fade-up">
+        
+        <div className='border-1 w-3/4 lg:w-1/2  p-6 text-center rounded shadow'>
+         <img src={camara} alt='camarógrafos docentemás' className='w-full h-40 object-cover rounded '/>
+        <h2 className='text-lg font-semibold color mt-2'>Camarógrafos DocenteMás</h2>
+        </div>
+        <div className='border-1 w-3/4 lg:w-1/2  p-6 text-center rounded shadow'>
+         <img src={mascotas} alt='portal mascotas' className='w-full h-40 object-cover rounded '/>
+        <h2 className='text-lg font-semibold color mt-2'>Portal Mascotas</h2>
+        </div>
+        <div className='border-1 w-3/4 lg:w-1/2  p-6 text-center rounded shadow'>
+         <img src={snap} alt='snap' className='w-full h-40 object-cover rounded '/>
+        <h2 className='text-lg font-semibold color mt-2'>Snap</h2>
+        </div>
         </div> 
     </>
   )
