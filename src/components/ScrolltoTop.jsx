1ce5d34947f09b977f6f2928ae91c5aff7d21ade
hectrhcc <hcontrerascorvacho@gmail.com>
2025-05-17 15:52:01 -0400

    wtf

--- src/components/ScrolltoTop.jsx
+++ src/components/ScrolltoTop.jsx
@@ -12,6 +12,3 @@ const ScrollToTop = () => {
 };
 
 export default ScrollToTop;
-// This component will scroll to the top of the page whenever the location changes.
-// You can use it in your main App component or any other component where you want this behavior.
-// To use this component, simply import it and include it in your main App component or any other component where you want this behavior.
