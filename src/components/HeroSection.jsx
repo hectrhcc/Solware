fe9ecfbb7c183ffba82a3c6fd1da57e5a95c4522
hectrhcc <hcontrerascorvacho@gmail.com>
2025-05-16 22:23:22 -0400

    efectos

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,26 +6,26 @@ export default function HeroSection() {
     <section className="hero-320-fix hero min-h-screen mb-0 pb-0 w-full bg-white px-6 flex flex-row items-center justify-between gap-0 lg:gap-20 md:gap-9 overflow-hidden">
 
       {/* Texto a la izquierda */}
-      <div className="w-1/2 md:w-2/3 lg:w-1/2 text-left  ">
-        <h1 className="hero-320-fix text-3xl lg:relative md:left-8 md:relative lg:left-30 lg:bottom-1 md:text-4xl lg:text-6xl font-bold text-gray-900 leading-tight">
+      <div className="w-1/2 md:w-2/3 lg:w-1/2 text-left " >
+        <h1 className="hero-320-fix text-3xl lg:relative md:left-8 md:relative lg:left-30 lg:bottom-1 md:text-4xl lg:text-6xl font-bold text-gray-900 leading-tight" data-aos="fade-up">
           Desarrollamos el <span className="text-[#0D206C]">sitio web </span><br className='lg:hidden'/>
           que tu negocio necesita
         </h1>
-        <p className="hero-320-fix mt-4 text-md md:w-9/10 md:text-xl lg:text-2xl lg:bottom-1 md:left-8 md:relative  text-gray-600 lg:relative lg:left-30">
+        <p className="hero-320-fix mt-4 text-md md:w-9/10 md:text-xl lg:text-2xl lg:bottom-1 md:left-8 md:relative  text-gray-600 lg:relative lg:left-30" data-aos="fade-up"> 
           Creamos sitios informativos, tiendas online y plataformas a medida para potenciar tu presencia digital.
         </p>
 
         {/* Botones */}
-        <button className="hero-320-fix hidden lg:block lg:relative lg:left-30 lg:bottom-4 mt-6 bg-indigo-600 text-white px-6 py-3 rounded-full text-base md:text-lg hover:bg-indigo-700 transition">
+        <button className="hero-320-fix hidden lg:block lg:relative lg:left-30 lg:bottom-4 mt-6 bg-indigo-600 text-white px-6 py-3 rounded-full text-base md:text-lg hover:bg-indigo-700 transition" data-aos="fade-up">
           Lleva tu empresa al siguiente nivel
         </button>
-        <button className="block md:left-8 md:relative  lg:hidden mt-6 bg-indigo-600 text-white px-6 py-3 rounded-full text-base md:text-lg hover:bg-indigo-700 transition">
+        <button className="block md:left-8 md:relative  lg:hidden mt-6 bg-indigo-600 text-white px-6 py-3 rounded-full text-base md:text-lg hover:bg-indigo-700 transition" data-aos="fade-up">
           VER SERVICIOS
         </button>
       </div>
 
       {/* Logo a la derecha */}
-      <div className="relative left-91  md:left-130 w-1/2 md:w-1/3 lg:w-1/2 h-full flex justify-end">
+      <div className="relative left-91  md:left-130 w-1/2 md:w-1/3 lg:w-1/2 h-full flex justify-end" data-aos="zoom-in-left">
         <img
           src={solware}
           alt="Solware"
--- src/components/Mein.jsx
+++ src/components/Mein.jsx
@@ -10,7 +10,7 @@ return(
     <h3 className="text-2xl text-left lg:text-4xl font-semibold mb-2 color lg:text-center lg:mb-10">Nuestra Misión</h3>
     <div className='flex flex-col lg:flex-row-reverse lg:gap-8 lg:w-full '>
       
-      <p className="text-md text-gray-700 text-justify mb-3 mt-2 lg:w-1/2 lg:text-2xl">
+      <p className="text-md text-gray-700 text-justify mb-3 mt-2 lg:w-1/2 lg:text-2xl"  data-aos="fade-left">
   En Solware, ayudamos a personas y empresas a conectar con el mundo digital a través de soluciones web modernas, funcionales y personalizadas. Nos comprometemos a entregar un servicio cercano, transparente y de calidad, adaptado a las necesidades reales de nuestros clientes.
       </p>
        <video
@@ -20,7 +20,8 @@ return(
   playsInline
   className='w-auto lg:h-65 mt-3 lg:w-1/2  '
   style={{ borderRadius: '1.3rem'}}
-
+ 
+   data-aos="fade-right"
 >
   <source
     src="https://videos.pexels.com/video-files/5572860/5572860-sd_640_360_30fps.mp4"
@@ -32,7 +33,7 @@ return(
     
       <h3 className="text-2xl lg:text-4xl text-left font-semibold mb-2 color lg:text-center lg:mb-10 lg:mt-14">Nuestra Visión</h3>
  <div className='flex flex-col lg:flex-row lg:gap-8 lg:w-full '>
-      <p className="text-md text-gray-700 text-justify mb-3 mt-2 lg:w-1/2 lg:text-2xl">
+      <p className="text-md text-gray-700 text-justify mb-3 mt-2 lg:w-1/2 lg:text-2xl" data-aos="fade-right">
         Ser una empresa reconocida en Chile por democratizar el acceso a tecnologías web, impulsando el crecimiento digital de emprendedores, pymes y profesionales con soluciones efectivas, asequibles y bien diseñadas.
       </p>
       <video
@@ -42,7 +43,7 @@ return(
   playsInline
   className='w-full lg:h-65 mt-3 lg:w-1/2 '
   style={{ borderRadius: '1.3rem'}}
-
+ data-aos="fade-left"
 >
   <source
     src="https://videos.pexels.com/video-files/5977258/5977258-sd_640_360_25fps.mp4"
